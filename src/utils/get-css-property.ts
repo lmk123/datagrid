3053311391451232b7443d6dad4c4135a70ea5bf
lmk123 <me@limingkai.cn>
2017-12-05 12:19:23 +0800

    改进代码

--- src/utils/get-css-property.ts
+++ src/utils/get-css-property.ts
@@ -5,14 +5,13 @@ export type CSSProperty = keyof CSSStyleDeclaration
 const { style } = document.createElement('div')
 
 type Vendor = 'webkit' | 'ms' | 'moz' | 'o'
-let vendors: Vendor[] = ['webkit', 'ms', 'moz', 'o']
 let vd: Vendor
 
 export default memory((property: CSSProperty) => {
   if (property in style) return property
   const camelCase = property[0].toUpperCase() + property.slice(1)
 
-  function getVendorProperty(vendor: Vendor) {
+  const getVendorProperty = (vendor: Vendor) => {
     const vendorProperty = (vendor + camelCase) as CSSProperty
     if (vendorProperty in style) {
       return vendorProperty
@@ -24,13 +23,11 @@ export default memory((property: CSSProperty) => {
   }
 
   let result: CSSProperty | undefined
-  vendors.some(vendor => {
-    const property = getVendorProperty(vendor)
-    if (property) {
-      result = property
+  ;(['webkit', 'ms', 'moz', 'o'] as Vendor[]).some(vendor => {
+    const vendorProperty = getVendorProperty(vendor)
+    if (vendorProperty) {
+      result = vendorProperty
       vd = vendor
-      //@ts-ignore 释放内存
-      vendors = null
       return true
     }
     return false
