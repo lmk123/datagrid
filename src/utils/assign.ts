e04bb8c81beff30dfeb5b38df1cd7e5ed371fe00
lmk123 <me@limingkai.cn>
2017-12-12 14:30:53 +0800

    重命名 forIn 为 forOwn

--- src/utils/assign.ts
+++ src/utils/assign.ts
@@ -1,6 +1,6 @@
 const hasOwnProperty = Object.prototype.hasOwnProperty
 
-export function forIn(obj: any, cb: (val: any, key: string) => void) {
+export function forOwn(obj: any, cb: (val: any, key: string) => void) {
   for (const key in obj) {
     if (hasOwnProperty.call(obj, key)) {
       cb(obj[key], key)
@@ -11,7 +11,7 @@ export function forIn(obj: any, cb: (val: any, key: string) => void) {
 export default Object.assign ||
   function(target: any, ...args: any[]) {
     args.forEach(arg => {
-      forIn(arg, (val, key) => {
+      forOwn(arg, (val, key) => {
         target[key] = val
       })
     })
